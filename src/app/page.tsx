0a2e3d41f2099c11dea3dc017e360938a8337e0a
mgt581 <alexbryant3234@gmail.com>
2026-07-11 05:43:56 +0100

    remove dupilcates index.html pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('/legacy/index.html')
+  redirect(process.env.NEXT_PUBLIC_FRONTEND_URL || 'https://mgt581.github.io/bds-site/')
 }
